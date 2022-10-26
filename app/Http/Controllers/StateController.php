8bb5fb7f4d5814ed2d492588182b233efea8b66f
CaiqueParra2001 <caiquepgparra@gmail.com>
2022-10-26 20:11:41 -0300

    1envio

---
+++ app/Http/Controllers/StateController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\State;
+use Illuminate\Http\Request;
+
+class StateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return State::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        State::create([
+            'name' => $request->name,
+            'uf' => $request->state_id,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return State::find("id", $id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        State::update([
+            'name' => $request->name,
+            'uf' => $request->uf,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    { {
+            $State = State::find("id", $id);
+            $State->delete();
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("Citys", [CityController::class,"index"]);
+Route::post("city", [CityController::class,"store"]);
+Route::get("city/{id}", [CityController::class,"show"]);
+Route::put("city/{id}", [CityController::class,"update"]);
+Route::delete("city/{id}", [CityController::class,"destroy"]);
+
+Route::get("States", [StateController::class,"index"]);
+Route::post("state", [StateController::class,"store"]);
+Route::get("state/{id}", [StateController::class,"show"]);
+Route::put("state/{id}", [StateController::class,"update"]);
+Route::delete("state/{id}", [StateController::class,"destroy"]);
